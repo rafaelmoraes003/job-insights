052ab9b272ab16a1c03c7a679015c22b63f97f78
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-06 17:56:42 -0300

    feat: implementa a função read, que é responsável por abrir o arquivo CSV e retornar os dados no formato de uma lista de dicionários.

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,40 +1,21 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    try:
+        with open(path, mode="r", encoding="utf-8") as file:
+            content = csv.DictReader(file, delimiter=",", quotechar='"')
+            content_list = [row for row in content]
+        return content_list
+    except OSError:
+        raise FileNotFoundError("File not found")
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    pass
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
