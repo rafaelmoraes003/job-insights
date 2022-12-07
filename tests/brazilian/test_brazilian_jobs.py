897add9efafb0bac0e7f749d7628e8df9fecb14f
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 18:57:27 -0300

    test: adiciona teste de arquivo não existente para a função test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,3 +1,4 @@
+import pytest
 from src.pre_built.brazilian_jobs import read_brazilian_file
 
 file = "tests/mocks/brazilians_jobs.csv"
@@ -7,13 +8,18 @@ keys_in_english = ["title", "salary", "type"]
 
 
 def test_brazilian_jobs():
-    # VERIFICA SE, PARA CADA TRABALHO DA LISTA,
-    # AS CHAVES EM INGLÊS ESTÃO PRESENTES E
-    # AS CHAVES EM PORTUGUÊS NÃO
+    # CHECK IF, FOR EACH JOB IN THE LIST,
+    # ENGLISH KEYS ARE PRESENT AND
+    # THE KEYS IN PORTUGUESE ARE NOT
 
     brazilian_jobs = read_brazilian_file(file)
     for job in brazilian_jobs:
         for key_in_english in keys_in_english:
-            assert(key_in_english in job) is True
+            assert (key_in_english in job) is True
         for key_in_portuguese in keys_in_portuguese:
-            assert(key_in_portuguese not in job) is True
+            assert (key_in_portuguese not in job) is True
+
+    # NON-EXISTENT FILE
+
+    with pytest.raises(FileNotFoundError):
+        read_brazilian_file("non-existent-file.txt")
