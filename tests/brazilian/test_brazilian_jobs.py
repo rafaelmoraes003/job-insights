ecffa6a3be16f53665e606ddcf87cef13215018a
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 02:13:59 -0300

    test: cria testes para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,19 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+file = "tests/mocks/brazilians_jobs.csv"
+
+keys_in_portuguese = ["titulo", "salario", "tipo"]
+keys_in_english = ["title", "salary", "type"]
 
 
 def test_brazilian_jobs():
-    pass
+    # VERIFICA SE, PARA CADA TRABALHO DA LISTA,
+    # AS CHAVES EM INGLÊS ESTÃO PRESENTES E
+    # AS CHAVES EM PORTUGUÊS NÃO
+
+    brazilian_jobs = read_brazilian_file(file)
+    for job in brazilian_jobs:
+        for key_in_english in keys_in_english:
+            assert(key_in_english in job) is True
+        for key_in_portuguese in keys_in_portuguese:
+            assert(key_in_portuguese not in job) is True
