1f4e956c4ef9ea42be6a0cac09b079e67b3a1f7c
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 01:37:49 -0300

    fix: identa arquivos

--- src/validators.py
+++ src/validators.py
@@ -2,9 +2,8 @@ from typing import Union, Dict
 
 
 def validate_salary(salary: Union[int, str]) -> None:
-    if(
-        (not isinstance(salary, int) and not isinstance(salary, str))
-        or (isinstance(salary, str) and not salary.isnumeric())
+    if (not isinstance(salary, int) and not isinstance(salary, str)) or (
+        isinstance(salary, str) and not salary.isnumeric()
     ):
         raise ValueError("Invalid salary.")
 
@@ -13,7 +12,7 @@ def validate_salaries_dict(job: Dict) -> None:
     max = "max_salary"
     min = "min_salary"
 
-    if(
+    if (
         (max not in job or min not in job)
         or (not str(job[max]).isnumeric() or not str(job[min]).isnumeric())
         or (int(job[min]) > int(job[max]))
