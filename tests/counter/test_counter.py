3083294e7526b43f85a2ce44c2cf7d26b49d6ea2
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 18:57:48 -0300

    fix: identação

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -6,21 +6,21 @@ file = "data/jobs.csv"
 
 def test_counter():
     # LOWER CASE
-    assert(count_ocurrences(file, "javascript")) == 122
-    assert(count_ocurrences(file, "python")) == 1639
+    assert (count_ocurrences(file, "javascript")) == 122
+    assert (count_ocurrences(file, "python")) == 1639
 
     # UPPER CASE
-    assert(count_ocurrences(file, "JAVASCRIPT")) == 122
-    assert(count_ocurrences(file, "PYTHON")) == 1639
+    assert (count_ocurrences(file, "JAVASCRIPT")) == 122
+    assert (count_ocurrences(file, "PYTHON")) == 1639
 
     # SHUFFLED
-    assert(count_ocurrences(file, "JavaScript")) == 122
-    assert(count_ocurrences(file, "Python")) == 1639
+    assert (count_ocurrences(file, "JavaScript")) == 122
+    assert (count_ocurrences(file, "Python")) == 1639
 
     # WRONG ASSERTIONS
-    assert(count_ocurrences(file, "javascript")) != 12345
-    assert(count_ocurrences(file, "python")) != 14
-    assert(count_ocurrences(file, "xablau")) == 0
+    assert (count_ocurrences(file, "javascript")) != 12345
+    assert (count_ocurrences(file, "python")) != 14
+    assert (count_ocurrences(file, "xablau")) == 0
 
     # NON-EXISTENT FILE
     with pytest.raises(FileNotFoundError):
