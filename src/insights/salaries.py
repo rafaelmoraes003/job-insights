3e1b89b49eaa1151dbee7b617f92128ec108966f
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 01:13:05 -0300

    feat: implementa a função matches_salary_range, que confere se o salário procurado está dentro da faixa salarial daquele emprego, retornando um booleano

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,5 +1,6 @@
 from typing import Union, List, Dict
 from src.insights.jobs import read
+from src.validators import validate_salaries_dict, validate_salary
 
 
 def get_salaries_set(type: str, jobs_list: list) -> set:
@@ -25,46 +26,12 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    validate_salaries_dict(job)
+    validate_salary(salary)
+    return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    pass
