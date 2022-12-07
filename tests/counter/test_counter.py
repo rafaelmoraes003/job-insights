25437a6208ef8b8649fc892c6efbdb2bda06354d
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 01:46:31 -0300

    test: adiciona mais asserções para os testes da função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -5,13 +5,23 @@ file = "data/jobs.csv"
 
 
 def test_counter():
-    assert (count_ocurrences(file, "javascript")) == 122
-    assert (count_ocurrences(file, "python")) == 1639
+    # LOWER CASE
+    assert(count_ocurrences(file, "javascript")) == 122
+    assert(count_ocurrences(file, "python")) == 1639
 
-    assert (count_ocurrences(file, "javascript")) != 22
-    assert (count_ocurrences(file, "python")) != 140
+    # UPPER CASE
+    assert(count_ocurrences(file, "JAVASCRIPT")) == 122
+    assert(count_ocurrences(file, "PYTHON")) == 1639
 
+    # SHUFFLED
+    assert(count_ocurrences(file, "JavaScript")) == 122
+    assert(count_ocurrences(file, "Python")) == 1639
+
+    # WRONG ASSERTIONS
+    assert(count_ocurrences(file, "javascript")) != 12345
+    assert(count_ocurrences(file, "python")) != 14
     assert(count_ocurrences(file, "xablau")) == 0
 
+    # NON-EXISTENT FILE
     with pytest.raises(FileNotFoundError):
         count_ocurrences("non-existent-file.txt", "duck")
