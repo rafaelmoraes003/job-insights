657b1f0821d3d2b5e75afa05deebe266a5672bee
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 19:49:59 -0300

    feat: adiciona listas de testes a um fixture do pytest

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,117 +2,133 @@ import pytest
 from src.pre_built.sorting import sort_by
 
 
-jobs = [
-    {
-        "id": 1,
-        "max_salary": 3000,
-        "min_salary": 1000,
-        "date_posted": "2022-12-07",
-    },
-    {
-        "id": 2,
-        "max_salary": 1000,
-        "min_salary": 800,
-        "date_posted": "2022-12-06",
-    },
-    {
-        "id": 3,
-        "max_salary": 5000,
-        "min_salary": 4500,
-        "date_posted": "2022-12-12",
-    },
-]
+@pytest.fixture
+def jobs():
+    return [
+        {
+            "id": 1,
+            "max_salary": 3000,
+            "min_salary": 1000,
+            "date_posted": "2022-12-07",
+        },
+        {
+            "id": 2,
+            "max_salary": 1000,
+            "min_salary": 800,
+            "date_posted": "2022-12-06",
+        },
+        {
+            "id": 3,
+            "max_salary": 5000,
+            "min_salary": 4500,
+            "date_posted": "2022-12-12",
+        },
+    ]
 
 
-jobs_after_sort_by_max_salary = [
-    {
-        "id": 3,
-        "max_salary": 5000,
-        "min_salary": 4500,
-        "date_posted": "2022-12-12",
-    },
-    {
-        "id": 1,
-        "max_salary": 3000,
-        "min_salary": 1000,
-        "date_posted": "2022-12-07",
-    },
-    {
-        "id": 2,
-        "max_salary": 1000,
-        "min_salary": 800,
-        "date_posted": "2022-12-06",
-    },
-]
+@pytest.fixture
+def jobs_after_sort_by_max_salary():
+    return [
+        {
+            "id": 3,
+            "max_salary": 5000,
+            "min_salary": 4500,
+            "date_posted": "2022-12-12",
+        },
+        {
+            "id": 1,
+            "max_salary": 3000,
+            "min_salary": 1000,
+            "date_posted": "2022-12-07",
+        },
+        {
+            "id": 2,
+            "max_salary": 1000,
+            "min_salary": 800,
+            "date_posted": "2022-12-06",
+        },
+    ]
 
 
-jobs_after_sort_by_min_salary = [
-    {
-        "id": 2,
-        "max_salary": 1000,
-        "min_salary": 800,
-        "date_posted": "2022-12-06",
-    },
-    {
-        "id": 1,
-        "max_salary": 3000,
-        "min_salary": 1000,
-        "date_posted": "2022-12-07",
-    },
-    {
-        "id": 3,
-        "max_salary": 5000,
-        "min_salary": 4500,
-        "date_posted": "2022-12-12",
-    },
-]
+@pytest.fixture
+def jobs_after_sort_by_min_salary():
+    return [
+        {
+            "id": 2,
+            "max_salary": 1000,
+            "min_salary": 800,
+            "date_posted": "2022-12-06",
+        },
+        {
+            "id": 1,
+            "max_salary": 3000,
+            "min_salary": 1000,
+            "date_posted": "2022-12-07",
+        },
+        {
+            "id": 3,
+            "max_salary": 5000,
+            "min_salary": 4500,
+            "date_posted": "2022-12-12",
+        },
+    ]
 
 
-jobs_after_wrong_sort_by_min_salary = [
-    {
-        "id": 3,
-        "max_salary": 5000,
-        "min_salary": 4500,
-        "date_posted": "2022-12-12",
-    },
-    {
-        "id": 1,
-        "max_salary": 3000,
-        "min_salary": 1000,
-        "date_posted": "2022-12-07",
-    },
-    {
-        "id": 2,
-        "max_salary": 1000,
-        "min_salary": 800,
-        "date_posted": "2022-12-06",
-    },
-]
+@pytest.fixture
+def jobs_after_wrong_sort_by_min_salary():
+    return [
+        {
+            "id": 3,
+            "max_salary": 5000,
+            "min_salary": 4500,
+            "date_posted": "2022-12-12",
+        },
+        {
+            "id": 1,
+            "max_salary": 3000,
+            "min_salary": 1000,
+            "date_posted": "2022-12-07",
+        },
+        {
+            "id": 2,
+            "max_salary": 1000,
+            "min_salary": 800,
+            "date_posted": "2022-12-06",
+        },
+    ]
 
 
-jobs_after_sort_by_date_posted = [
-    {
-        "id": 3,
-        "max_salary": 5000,
-        "min_salary": 4500,
-        "date_posted": "2022-12-12",
-    },
-    {
-        "id": 1,
-        "max_salary": 3000,
-        "min_salary": 1000,
-        "date_posted": "2022-12-07",
-    },
-    {
-        "id": 2,
-        "max_salary": 1000,
-        "min_salary": 800,
-        "date_posted": "2022-12-06",
-    },
-]
+@pytest.fixture
+def jobs_after_sort_by_date_posted():
+    return [
+        {
+            "id": 3,
+            "max_salary": 5000,
+            "min_salary": 4500,
+            "date_posted": "2022-12-12",
+        },
+        {
+            "id": 1,
+            "max_salary": 3000,
+            "min_salary": 1000,
+            "date_posted": "2022-12-07",
+        },
+        {
+            "id": 2,
+            "max_salary": 1000,
+            "min_salary": 800,
+            "date_posted": "2022-12-06",
+        },
+    ]
 
 
-def test_sort_by_criteria():
+def test_sort_by_criteria(
+    jobs,
+    jobs_after_sort_by_max_salary,
+    jobs_after_sort_by_min_salary,
+    jobs_after_wrong_sort_by_min_salary,
+    jobs_after_sort_by_date_posted
+):
     my_jobs = jobs
 
     # SORT MY MAX_SALARY
