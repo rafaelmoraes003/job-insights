3ff23003e8d1923c32c6b98152e89a3ee80d9453
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-07 01:11:51 -0300

    feat: cria função validate_salaries_dict, que recebe um dicionário de salários e faz validação dos valores

--- src/validators.py
+++ src/validators.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Dict
 
 
 def validate_salary(salary: Union[int, str]) -> None:
@@ -7,3 +7,15 @@ def validate_salary(salary: Union[int, str]) -> None:
         or (isinstance(salary, str) and not salary.isnumeric())
     ):
         raise ValueError("Invalid salary.")
+
+
+def validate_salaries_dict(job: Dict) -> None:
+    max = "max_salary"
+    min = "min_salary"
+
+    if(
+        (max not in job or min not in job)
+        or (not str(job[max]).isnumeric() or not str(job[min]).isnumeric())
+        or (int(job[min]) > int(job[max]))
+    ):
+        raise ValueError("Invalid salaries dict.")
